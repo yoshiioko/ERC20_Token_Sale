972c0a559f3c900fa653c6b7824b55b08f8edd46
yoshiioko <acardoza@pdx.edu>
2022-05-09 22:52:46 -0700

    minor fix

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -16,7 +16,7 @@ contract("Token Test", async (accounts) => {
   const [initialHolder, recipient, anotherAccount] = accounts;
 
   beforeEach(async () => {
-    this.instance = await Token.deployed();
+    this.instance = await Token.new(1000);
   });
 
   it("All tokens should be in my account", async () => {
@@ -28,18 +28,17 @@ contract("Token Test", async (accounts) => {
 
   it("I can send tokens from Account 1 to Account 2", async () => {
     const sendTokens = 1;
-    let instance = await Token.deployed();
-    let totalSupply = await instance.totalSupply();
+    let totalSupply = await this.instance.totalSupply();
     await expect(
-      instance.balanceOf(initialHolder)
+      this.instance.balanceOf(initialHolder)
     ).to.eventually.be.a.bignumber.equal(totalSupply);
-    await expect(instance.transfer(recipient, sendTokens)).to.eventually.be
+    await expect(this.instance.transfer(recipient, sendTokens)).to.eventually.be
       .fulfilled;
     await expect(
-      instance.balanceOf(initialHolder)
+      this.instance.balanceOf(initialHolder)
     ).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
     await expect(
-      instance.balanceOf(recipient)
+      this.instance.balanceOf(recipient)
     ).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
   });
 
