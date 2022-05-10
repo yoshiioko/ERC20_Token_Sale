855cb619cb2557aac1bb7eb78667a1774aeb66ec
yoshiioko <acardoza@pdx.edu>
2022-05-09 17:53:56 -0700

    initial code for contract

---
+++ contracts/MyToken.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    /**
+     * @dev Constructor for MyToken contract. Initializes initial supply and decimal places.
+     * Also configures the token name and symbol.
+     */
+    constructor(uint256 initialSupply) ERC20("StarDuck Capu-Token", "SCT") public {
+        _mint(msg.sender, initialSupply);
+        _setupDecimals(0);
+    }
+}
+
