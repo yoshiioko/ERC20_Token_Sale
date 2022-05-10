df47f9ef9e3cd1c5ef3596d4744570b4ad83162c
yoshiioko <acardoza@pdx.edu>
2022-05-09 17:56:02 -0700

    underscores added for readibility

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var MyToken = artifacts.require("MyToken.sol");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(MyToken, 1000000000);
+  await deployer.deploy(MyToken, 1_000_000_000);
 };
