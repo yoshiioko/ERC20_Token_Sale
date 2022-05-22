c6011b3032d1a0503d68cadf5a616d2bb95218fc
yoshiioko <acardoza@pdx.edu>
2022-05-22 13:39:06 -0700

    update for deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require("dotenv").config({ path: "./.env" });
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MetaMastAccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +12,16 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
+    },
+    ganache_local: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "http://127.0.0.1:8545",
+          MetaMastAccountIndex
+        );
+      },
+      network_id: 5777,
     },
   },
   compilers: {
